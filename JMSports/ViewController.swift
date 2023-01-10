818e07aa0f944676dfdeedd7523b9c57a92c9195
Jobson Mateus <jobson@klever.io>
2023-01-10 15:07:59 -0300

    Add layers

--- JMSports/ViewController.swift
+++ JMSports/ViewController.swift
@@ -6,6 +6,8 @@
 //
 
 import UIKit
+import DataLayer
+import DomainLayer
 
 class ViewController: UIViewController {
 
---
+++ UILayer/UILayer.h
@@ -0,0 +1,18 @@
+//
+//  UILayer.h
+//  UILayer
+//
+//  Created by Jobson Mateus on 10/01/23.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for UILayer.
+FOUNDATION_EXPORT double UILayerVersionNumber;
+
+//! Project version string for UILayer.
+FOUNDATION_EXPORT const unsigned char UILayerVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <UILayer/PublicHeader.h>
+
+
