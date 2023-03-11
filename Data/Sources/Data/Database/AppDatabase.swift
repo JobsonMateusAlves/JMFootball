d527a39deaa9010e9a4eb511615d178f8334a093
Jobson Mateus <jobson@klever.io>
2023-03-11 03:51:32 -0300

    Add Database

--- Data/Sources/Data/Database/AppDatabase.swift
+++ Data/Sources/Data/Database/AppDatabase.swift
@@ -6,3 +6,42 @@
 //
 
 import Foundation
+import GRDB
+
+public final class AppDatabase {
+    static var shared: AppDatabase?
+    
+    var dbQueue: DatabaseQueue
+    var leagueDatabase: LeagueDatabase
+    
+    private init() throws {
+        let url = try AppDatabase.getDirectoryURL()
+        dbQueue = try DatabaseQueue(path: url.path)
+        leagueDatabase = LeagueDatabaseImpl(dbQueue: dbQueue)
+        
+        debugPrint("DATABASE: \(url.path)")
+    }
+    
+    public static func initialize() {
+        shared = try? AppDatabase()
+    }
+    
+    private static func getDirectoryURL() throws -> URL {
+        let fileManager: FileManager = FileManager()
+        let folderURL: URL = try fileManager
+            .url(
+                for: .applicationSupportDirectory,
+                in: .userDomainMask,
+                appropriateFor: nil,
+                create: true
+            )
+            .appendingPathComponent(
+                "database",
+                isDirectory: true
+            )
+        
+        try fileManager.createDirectory(at: folderURL, withIntermediateDirectories: true)
+        return folderURL.appendingPathComponent("JMFootbalDB.sqlite")
+    }
+}
+
--- Data/Sources/Data/Database/Database.swift
+++ Data/Sources/Data/Database/Database.swift
@@ -1,8 +1,13 @@
 //
-//  File.swift
+//  Database.swift
 //  
 //
 //  Created by Jobson Mateus on 11/03/23.
 //
 
 import Foundation
+
+protocol Database {
+    func createTable() throws 
+}
+
